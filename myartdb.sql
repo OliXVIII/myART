186b2d170d7830420bfca99a5df12573457ec363
jean-christophe <jcpar27@ulaval.ca>
2023-03-18 14:24:10 -0400

    ajout des tables

--- myartdb.sql
+++ myartdb.sql
@@ -40,6 +40,50 @@ CREATE TABLE IF NOT EXISTS `arts` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS clients (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  mot_de_passe VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(255) NOT NULL UNIQUE,
+  description TEXT
+);
+CREATE TABLE IF NOT EXISTS administrateurs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS produits (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nom varchar(255) NOT NULL,
+    description TEXT,
+    prix DECIMAL(10,2) NOT NULL,
+    quantite INT NOT NULL,
+    categorie_id INT,
+    image_url VARCHAR(255),
+    FOREIGN KEY (categorie_id) REFERENCES categories(id)
+);
+CREATE TABLE IF NOT EXISTS paniers (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  client_id INT,
+  FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+CREATE TABLE IF NOT EXISTS commandes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    client_id INT,
+    date_commande TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    statut ENUM('En attente', 'En cours', 'Expédié', 'Livré', 'Annulé') NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+
+
 --
 -- Dumping data for table `posts`
 --
