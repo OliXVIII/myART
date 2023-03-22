c29eb72d512f8f3dafe6223d8c98748f327aeb03
jparent1010 <jcpar27@ulaval,ca>
2023-03-22 12:39:53 -0400

    Pouffinement des tables

--- myartdb.sql
+++ myartdb.sql
@@ -1,36 +1,19 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Apr 26, 2020 at 08:12 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "-05:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `personalblog`
---CREATE DATABASE IF NOT EXISTS baseDeDonnees;
+CREATE DATABASE IF NOT EXISTS baseDeDonnees;
 USE baseDeDonnees;
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-
+CREATE TABLE IF NOT EXISTS adresses(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pays VARCHAR(255) NOT NULL,
+    code_postale VARCHAR(10) NOT NULL,
+    ville VARCHAR(255) NOT NULL,
+    rue VARCHAR(255) NOT NULL,
+    numero_porte INT
+);
 
 CREATE TABLE IF NOT EXISTS clients (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -44,12 +27,14 @@ CREATE TABLE IF NOT EXISTS categories (
   nom VARCHAR(255) NOT NULL UNIQUE,
   description TEXT
 );
+
 CREATE TABLE IF NOT EXISTS administrateurs (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(255) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
 );
+
 CREATE TABLE IF NOT EXISTS produits (
     id int PRIMARY KEY AUTO_INCREMENT,
     nom varchar(255) NOT NULL,
@@ -60,6 +45,7 @@ CREATE TABLE IF NOT EXISTS produits (
     image_url VARCHAR(255),
     FOREIGN KEY (categorie_id) REFERENCES categories(id)
 );
+
 CREATE TABLE IF NOT EXISTS paniers (
   id INT PRIMARY KEY AUTO_INCREMENT,
   client_id INT,
@@ -69,9 +55,11 @@ CREATE TABLE IF NOT EXISTS paniers (
 CREATE TABLE IF NOT EXISTS commandes (
     id INT PRIMARY KEY AUTO_INCREMENT,
     client_id INT,
+    adresse_id INT,
     date_commande TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     statut ENUM('En attente', 'En cours', 'Expédié', 'Livré', 'Annulé') NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients(id)
+    FOREIGN KEY (client_id) REFERENCES clients(id),
+    FOREIGN KEY (adresse_id) REFERENCES adresses(id)
 );
 
 CREATE TABLE IF NOT EXISTS artistes(
@@ -79,7 +67,9 @@ CREATE TABLE IF NOT EXISTS artistes(
     nom VARCHAR(255) NOT NULL,
     nationalite VARCHAR(255) NOT NULL,
     anneeDeNaissance DATE,
-    bibliographie text
+    bibliographie text,
+    produit_id INT,
+    FOREIGN KEY (produit_id) REFERENCES produits(id)
 );
 
 CREATE TABLE IF NOT EXISTS paiements(
@@ -87,16 +77,9 @@ CREATE TABLE IF NOT EXISTS paiements(
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     type ENUM('argent','crédit','débit','autre'),
     montant DECIMAL(10, 2) NOT NULL,
-    statut ENUM('En cours','Complété','Échec')
-);
-
-CREATE TABLE IF NOT EXISTS adresses(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    pays VARCHAR(255) NOT NULL,
-    code_postale VARCHAR(10) NOT NULL,
-    ville VARCHAR(255) NOT NULL,
-    rue VARCHAR(255) NOT NULL,
-    numero_porte INT
+    statut ENUM('En cours','Complété','Échec'),
+    commande_id INT,
+    FOREIGN KEY (commande_id) REFERENCES commandes(id)
 );
 
 CREATE TABLE IF NOT EXISTS lignePanier(
@@ -108,13 +91,12 @@ CREATE TABLE IF NOT EXISTS lignePanier(
     FOREIGN KEY(id_produit) REFERENCES produits(id)
 );
 
-
-
---
--- Dumping data for table `posts`
---
-
--- -------------------------------------------------------
+CREATE TABLE IF NOT EXISTS comptes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date_creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_client INT NOT NULL,
+    FOREIGN KEY(id_client) REFERENCES clients(id)
+);
 
 INSERT INTO clients (nom, email, mot_de_passe) VALUES("Jean-Christophe Parent","jcpar27@ulaval.ca","salut11");
 SELECT * FROM clients;
