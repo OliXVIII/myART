10d84c3a6cdbe0f8dafd35aa0ff082bbdd057539
jparent1010 <jcpar27@ulaval,ca>
2023-04-12 22:39:37 -0400

    on a maintenant des routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.scss";
 import { Header } from "./components/header/header";
 import { Homepage } from "./components/homepage/homepage";
+import ArtistList from './components/ArtistList';
+import PaymentPage from './components/PaymentPage';
 
 function App() {
   const [data, setData] = useState({
@@ -11,23 +13,26 @@ function App() {
 
   useEffect(() => {
     fetch("http://localhost:3001/").then((res) =>
-      res.json().then((data) => {
-        setData({
-          title: data.title,
-          description: data.description,
-        });
-      })
+        res.json().then((data) => {
+          setData({
+            title: data.title,
+            description: data.description,
+          });
+        })
     );
   }, []);
 
   return (
-    <div className="App">
-      <Header />
-      <Homepage />
-        <ArtistList/>
-      <Filter />
-    </div>
+      <Router>
+        <div className="App">
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Homepage}/>
+            <Route path="/artistes" component={ArtistList}/>
+            <Route path="/paiement" component={PaymentPage}/>
+          </Switch>
+        </div>
+      </Router>
   );
 }
-
 export default App;
